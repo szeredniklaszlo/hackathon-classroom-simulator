390549933455155a41cb8c67c5caead8c7f48738
S4x4p <116021839+S4x4p@users.noreply.github.com>
2026-02-28 01:08:48 +0100

    Add student editor

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -5,6 +5,7 @@ interface AppState {
     students: Student[];
     classes: VirtualClass[];
     addStudent: (student: Student) => void;
+    updateStudent: (student: Student) => void;
     setStudents: (students: Student[]) => void;
     addClass: (newClass: VirtualClass) => void;
     setClasses: (classes: VirtualClass[]) => void;
@@ -16,6 +17,10 @@ export const useStore = create<AppState>((set) => ({
 
     addStudent: (student) => set((state) => ({
         students: [...state.students, student]
+    })),
+
+    updateStudent: (student) => set((state) => ({
+        students: state.students.map((s) => (s.id === student.id ? student : s))
     })),
 
     setStudents: (students) => set({ students }),
